5dab11e7dfbf0d82c0e25de03a029fee8442d9ac
shamiul <anik.savar.bd@gmail.com>
2023-05-03 20:13:42 +0600

    Header image added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,6 +5,21 @@ const Home = () => {
   return (
     <div>
       <HomeBanner></HomeBanner>
+
+      <div className="max-w-7xl mx-auto mt-12 lg:mt-20 p-4">
+
+        
+
+        <div className="p-4 lg:p-8 rounded-lg box-shadow-custom mt-6">
+          <header>
+            <h2 className="content-title text-center">Subscribe to Our Newsletter</h2>
+          </header>
+          <p className='content-description text-center'>Subscribe to our newsletter & keep up with our latest recipes and organized workshops.</p>
+          
+
+        </div>
+
+      </div>
     </div>
   );
 };
--- src/pages/shared/Header/Header.jsx
+++ src/pages/shared/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Header.css';
 import { Link, NavLink } from 'react-router-dom';
+import ProfilePhoto from '../../../assets/images/profile.png';
 import { TbChefHat } from "react-icons/tb";
 
 const Header = () => {
@@ -29,6 +30,7 @@ const Header = () => {
 						Chef World
 					</Link>
 				</div>
+
 				<div className="navbar-end hidden lg:flex">
 					<ul className="flex gap-10 text-xl font-semibold menu-horizontal px-1">
 						<li className="nav-item hover:cursor-pointer">
@@ -41,6 +43,35 @@ const Header = () => {
 							<Link to="/login" className="primary-button-md">Login</Link>
 						</li>
 					</ul>
+
+					{/* Profile Image */}
+					<div className="dropdown dropdown-end mt-1">
+						<label tabIndex={0} className="btn btn-ghost btn-circle avatar tooltip tooltip-bottom" data-tip="Name">
+							<div className="w-10 rounded-full ring-2 ring-offset-2 ring-slate-400">
+								<img className='object-top' src={ProfilePhoto} />
+							</div>
+						</label>
+						<ul tabIndex={0} className="mt-3 p-3 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
+							{/* <li>
+								<a className="justify-between">
+									Profile
+									<span className="badge">New</span>
+								</a>
+							</li> */}
+							<div className='w-full flex justify-center'>
+								<div className="mt-2 mb-3 h-16 w-16 rounded-full ring-2 ring-offset-2 ring-slate-400">
+									<img className='h-16 w-16 aspect-square rounded-full object-center' src={ProfilePhoto} />
+								</div>
+							</div>
+							
+							<li className='mt-1 text-center'>Hello, User</li>
+							<div className="divider mt-1 mb-2"></div>
+							<li>
+								<Link to="/login" className="bg-red-500 hover:bg-red-600 transition hover:delay-200 text-white font-bold py-2 justify-center">Logout</Link>
+							</li>
+						</ul>
+					</div>
+
 				</div>
 			</nav>
 		</div>
